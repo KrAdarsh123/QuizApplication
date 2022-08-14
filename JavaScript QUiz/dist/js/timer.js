f2e8e2cec8ec9539b48e8281a0e441218f702481
KrAdarsh123 <19cse286.kumaradarsh@giet.edu>
2022-08-14 17:13:09 +0530

    Third Commit

---
+++ JavaScript QUiz/dist/js/timer.js
@@ -0,0 +1,17 @@
+let seconds = 0;
+let minutes = 0;
+console.log(seconds, minutes);
+let time = 0;
+let mytime = setInterval(function(){
+        time++;
+        
+        if(seconds < 59) {
+            seconds++;
+        } else {
+            seconds = 0;
+            minutes++;
+        }
+        let formatted_sec = seconds < 10 ? `0${seconds}`: `${seconds}`;
+        let formatted_min = minutes < 10 ? `0${minutes}`: `${minutes}`
+        document.querySelector("span.time").innerHTML = `${formatted_min} : ${formatted_sec}`;
+    }, 1000);
